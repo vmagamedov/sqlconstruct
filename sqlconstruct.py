c0429b6e4a6247a2146df9ff34dac157921a52fd
Vladimir Magamedov <vladimir@magamedov.com>
2014-02-03 08:08:19 +0200

    Added initial implementation of the sub-queries support.

--- sqlconstruct.py
+++ sqlconstruct.py
@@ -37,13 +37,13 @@
 import sys
 import inspect
 from operator import attrgetter
-from functools import partial
-from itertools import chain
+from functools import partial, wraps
+from collections import defaultdict
 
 import sqlalchemy
 from sqlalchemy.sql import ColumnElement
-from sqlalchemy.util import immutabledict, ImmutableContainer
-from sqlalchemy.orm.query import _QueryEntity
+from sqlalchemy.util import immutabledict, ImmutableContainer, OrderedSet
+from sqlalchemy.orm.query import Query as _SAQuery, _QueryEntity
 from sqlalchemy.orm.attributes import QueryableAttribute
 
 
@@ -81,34 +81,173 @@ else:
 __all__ = ('Construct', 'if_', 'apply_', 'define', 'QueryMixin')
 
 
+class QueryPlan(object):
+
+    def __init__(self, session=None):
+        self._session = session
+        self._queries = OrderedSet({None})
+        self._columns = defaultdict(OrderedSet)
+        self._children = defaultdict(OrderedSet)
+
+    def add_query(self, query, parent_query):
+        self._queries.add(query)
+        self._children[parent_query].add(query)
+
+    def add_expr(self, query, column):
+        self._columns[query].add(column)
+
+    def query_id(self, query):
+        return list(self._queries).index(query)
+
+    def column_id(self, query, column):
+        return list(self._columns[query]).index(column)
+
+    def query_columns(self, query):
+        return tuple(self._columns.get(query) or ())
+
+    def query_children(self, query):
+        return tuple(self._children.get(query) or ())
+
+    def process_rows(self, rows):
+        children = self.query_children(None)
+        queries = (None,) + children
+        results = (rows,) + tuple(child.process(self, None, rows)
+                                  for child in children)
+        return {0: [
+            tuple(zip(map(self.query_id, queries),
+                      result_row))
+            for result_row in zip(*results)
+        ]}
+
+
+class Scope(object):
+
+    def __init__(self, query_plan, query=None, parent=None):
+        self.query_plan = query_plan
+        self.query = query
+        self.parent = parent
+
+        if query and parent:
+            for expr in query.__requires__():
+                self.query_plan.add_query(query, parent.query)
+                self.query_plan.add_expr(query, expr)
+
+    def lookup(self, column):
+        scope = self
+        while scope.query:
+            if column in scope.query:
+                return scope
+            scope = scope.parent
+        return scope
+
+    def nested(self, query):
+        ext_expr = query.__reference__()
+        scope = self.lookup(ext_expr)
+        self.query_plan.add_expr(scope.query, ext_expr)
+        return type(self)(self.query_plan, query, scope)
+
+    def add(self, column, query=None):
+        if query is None:
+            query = self.lookup(column).query
+        self.query_plan.add_expr(query, column)
+
+        query_id = self.query_plan.query_id(query)
+        column_id = self.query_plan.column_id(query, column)
+        def proc(result, _query_id=query_id, _column_id=column_id):
+            return result[_query_id][_column_id]
+        return proc
+
+    def gen_loop(self):
+        def loop(result, _query_id=self.query_plan.query_id(self.query)):
+            for item in result[_query_id]:
+                # TODO: optimize
+                yield dict(result, **dict(item))
+        return loop
+
+
+class QueryBase(object):
+
+    def __contains__(self, column):
+        raise NotImplementedError
+
+    def __reference__(self):
+        return None
+
+    def __requires__(self):
+        return tuple()
+
+    def process(self, query_plan, outer_query, outer_rows):
+        raise NotImplementedError
+
+
+class SubQuery(QueryBase):
+
+    def __init__(self, ext_expr, int_expr, query):
+        self._ext_expr = ext_expr
+        self._int_expr = int_expr
+        self._sa_query = query
+        self._hash = hash((type(self), self._ext_expr,
+                           self._int_expr, self._sa_query))
+
+    def __reference__(self):
+        return self._ext_expr
+
+    def __requires__(self):
+        return [self._int_expr]
+
+    def __hash__(self):
+        return self._hash
+
+    def __contains__(self, column):
+        return any(el.c.contains_column(column)
+                   for el in self._sa_query.statement.froms)
+
+    def process(self, query_plan, outer_query, outer_rows):
+        ext_col_id = query_plan.column_id(outer_query, self._ext_expr)
+        ext_exprs = [row[ext_col_id] for row in outer_rows]
+        if ext_exprs:
+            columns = query_plan.query_columns(self) + (self._int_expr,)
+            rows = (
+                self._sa_query
+                .with_session(query_plan._session.registry())
+                .with_entities(*columns)
+                .filter(self._int_expr.in_(ext_exprs))
+                .all()
+            )
+        else:
+            rows = []
+
+        children = query_plan.query_children(self)
+
+        queries = (self,) + children
+        results = (rows,) + tuple(child.process(query_plan, self, rows)
+                                  for child in children)
+
+        col_id = query_plan.column_id(self, self._int_expr)
+        groups = defaultdict(list)
+        for result_row in zip(*results):
+            groups[result_row[0][col_id]].append(
+                tuple(zip(map(query_plan.query_id, queries),
+                          result_row))
+            )
+        return [groups[ext_expr] for ext_expr in ext_exprs]
+
+
 class Processable(object):
 
-    def __columns__(self):
-        raise NotImplementedError
-
-    def __processor__(self):
+    def __processor__(self, scope):
         raise NotImplementedError
 
 
-def _get_value_processor(value):
+def _get_value_processor(scope, value):
     if isinstance(value, ColumnElement):
-        return lambda row_map, row, _hash=hash(value): row[row_map[_hash]]
+        return scope.add(value)
     elif isinstance(value, QueryableAttribute):
-        return _get_value_processor(value.__clause_element__())
+        return _get_value_processor(scope, value.__clause_element__())
     elif isinstance(value, Processable):
-        return value.__processor__()
+        return value.__processor__(scope)
     else:
-        return lambda row_map, row, _value=value: _value
-
-
-def _yield_columns(value):
-    if isinstance(value, ColumnElement):
-        yield value
-    elif isinstance(value, QueryableAttribute):
-        yield value.__clause_element__()
-    elif isinstance(value, Processable):
-        for column in value.__columns__():
-            yield column
+        return lambda result, _value=value: _value
 
 
 class Object(immutabledict):
@@ -133,19 +272,75 @@ class Object(immutabledict):
         return type(self), (dict(self),)
 
 
+def _proxy_query_method(unbound_method):
+    func = unbound_method.im_func
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        return func(self._query, *args, **kwargs)
+    return wrapper
+
+
+def _generative_proxy_query_method(unbound_method):
+    func = unbound_method.im_func
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        cls = type(self)
+        clone = cls.__new__(cls)
+        clone.__dict__.update(self.__dict__)
+        clone._query = func(clone._query, *args, **kwargs)
+        return clone
+    return wrapper
+
+
+def _generative_proxy_query_method2(unbound_method):
+    func = unbound_method.im_func
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        return func(self, *args, **kwargs)
+    return wrapper
+
+
+class ConstructQuery(object):
+
+    def __init__(self, session, spec):
+        self._session = session
+        self._spec = spec
+        self._keys, self._values = zip(*spec.items()) if spec else [(), ()]
+        self._scope = Scope(QueryPlan(session))
+        self._processors = [_get_value_processor(self._scope, val)
+                            for val in self._values]
+        self._query = _SAQuery(self._scope.query_plan.query_columns(None))
+
+    __str__     = _proxy_query_method(_SAQuery.__str__)
+
+    join        = _generative_proxy_query_method(_SAQuery.join)
+    outerjoin   = _generative_proxy_query_method(_SAQuery.outerjoin)
+    filter      = _generative_proxy_query_method(_SAQuery.filter)
+
+    def __iter__(self):
+        rows = self._query.with_session(self._session.registry()).all()
+        result = self._scope.query_plan.process_rows(rows)
+        for r in self._scope.gen_loop()(result):
+            yield Object(zip(self._keys, [proc(r) for proc in self._processors]))
+
+    def all(self):
+        return list(self)
+
+
 class Construct(Bundle):
     single_entity = True
 
     def __init__(self, spec):
         self._keys, self._values = zip(*spec.items()) if spec else [(), ()]
-        self._columns = tuple(set(chain(*_map(_yield_columns, self._values))))
-        self._processors = tuple(_map(_get_value_processor, self._values))
-        self._row_map = {hash(col): i for i, col in enumerate(self._columns)}
+        self._scope = Scope(QueryPlan())
+        self._processors = [_get_value_processor(self._scope, val)
+                            for val in self._values]
         self._range = tuple(_range(len(self._keys)))
+        self._columns = self._scope.query_plan.query_columns(None)
         super(Construct, self).__init__(None, *self._columns)
 
     def _from_row(self, row):
-        return Object({self._keys[i]: self._processors[i](self._row_map, row)
+        return Object({self._keys[i]: self._processors[i]([row])
                        for i in self._range})
 
     def from_query(self, query):
@@ -154,7 +349,7 @@ class Construct(Bundle):
 
     def create_row_processor(self, query, procs, labels):
         def proc(row, result):
-            return self._from_row([proc(row, None) for proc in procs])
+            return self._from_row(row)
         return proc
 
 
@@ -165,20 +360,15 @@ class if_(Processable):
         self._then = then_
         self._else = else_
 
-    def __columns__(self):
-        for obj in (self._cond, self._then, self._else):
-            for column in _yield_columns(obj):
-                yield column
-
-    def __processor__(self):
-        def process(row_map, row,
-                    cond_proc=_get_value_processor(self._cond),
-                    then_proc=_get_value_processor(self._then),
-                    else_proc=_get_value_processor(self._else)):
-            if cond_proc(row_map, row):
-                return then_proc(row_map, row)
+    def __processor__(self, scope):
+        def process(result,
+                    cond_proc=_get_value_processor(scope, self._cond),
+                    then_proc=_get_value_processor(scope, self._then),
+                    else_proc=_get_value_processor(scope, self._else)):
+            if cond_proc(result):
+                return then_proc(result)
             else:
-                return else_proc(row_map, row)
+                return else_proc(result)
         return process
 
 
@@ -189,31 +379,42 @@ class apply_(Processable):
         self._args = args or []
         self._kwargs = kwargs or {}
 
-    def __columns__(self):
-        for arg in set(self._args).union(self._kwargs.values()):
-            for column in _yield_columns(arg):
-                yield column
-
-    def __processor__(self):
+    def __processor__(self, scope):
         args = []
         eval_dict = {'__func__': self._func}
 
         for i, arg in enumerate(self._args):
-            args.append('__proc{0}__(row_map, row)'.format(i))
-            eval_dict['__proc{0}__'.format(i)] = _get_value_processor(arg)
+            args.append('__proc{0}__(result)'.format(i))
+            eval_dict['__proc{0}__'.format(i)] = _get_value_processor(scope, arg)
 
         for key, arg in self._kwargs.items():
-            args.append('{0}=__{0}_proc__(row_map, row)'.format(key))
-            eval_dict['__{0}_proc__'.format(key)] = _get_value_processor(arg)
+            args.append('{0}=__{0}_proc__(result)'.format(key))
+            eval_dict['__{0}_proc__'.format(key)] = _get_value_processor(scope, arg)
 
         processor_src = (
-            'def __processor__(row_map, row):\n'
+            'def __processor__(result):\n'
             '    return __func__({args})\n'
             .format(args=', '.join(args))
         )
         _exec_in(compile(processor_src, __name__, 'single'),
                  eval_dict)
         return eval_dict['__processor__']
+
+
+class map_(Processable):
+
+    def __init__(self, func, collection):
+        # collection.parent.relationships[collection.key]
+        self._func = func
+        self._sub_query = collection
+
+    def __processor__(self, scope):
+        nested_scope = scope.nested(self._sub_query)
+        func_proc = _get_value_processor(nested_scope, self._func)
+        loop = nested_scope.gen_loop()
+        def process(result):
+            return [func_proc(item) for item in loop(result)]
+        return process
 
 
 class _arg_helper(object):
