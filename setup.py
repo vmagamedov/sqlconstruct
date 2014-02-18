3a4c48f37ed1aece99c4b7be67b73625ed991aec
Vladimir Magamedov <vladimir@magamedov.com>
2014-02-18 18:23:02 +0200

    Updated setup.py and tox.ini files.

--- setup.py
+++ setup.py
@@ -11,13 +11,12 @@ with open(join(dirname(__file__), 'README.rst')) as f:
 
 setup(
     name='SQLConstruct',
-    version='0.1',
+    version='0.2',
     description='Functional approach to query database using SQLAlchemy',
     long_description=README,
     author='Vladimir Magamedov',
     author_email='vladimir@magamedov.com',
     url='http://github.com/vmagamedov/sqlconstruct',
     license='BSD',
-    install_requires=['SQLAlchemy>=0.7'],
     py_modules=['sqlconstruct'],
 )
